cecdd6f81cb484f692c9065d76fcace7468b6f85
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-08-26 08:35:06 +0100

    test: require v4.7.1 hotfix runtime version

--- tests/v47-runtime-version.test.js
+++ tests/v47-runtime-version.test.js
@@ -5,7 +5,7 @@ const path=require('node:path');
 
 const app=fs.readFileSync(path.join(__dirname,'..','src','v45-app.js'),'utf8');
 
-test('Faction v4.7 source advertises runtime version 4.7.0 before public release pinning',()=>{
-  assert.match(app,/SCRIPT_VERSION\s*=\s*'4\.7\.0'/);
+test('Faction v4.7.1 hotfix source advertises runtime version 4.7.1 before public release pinning',()=>{
+  assert.match(app,/SCRIPT_VERSION\s*=\s*'4\.7\.1'/);
   assert.match(app,/Recruitment Agency v4\.7 source started/);
 });
